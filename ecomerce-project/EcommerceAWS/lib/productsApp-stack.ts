fe923ec044f99c6ac1d81f76983300a2ce0f3a49
Mateuxx <motamatheus10@gmail.com>
2025-04-30 16:22:54 -0400

    Criação de uma tabela no dynamodb

--- ecomerce-project/EcommerceAWS/lib/productsApp-stack.ts
+++ ecomerce-project/EcommerceAWS/lib/productsApp-stack.ts
@@ -1,4 +1,5 @@
-import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import { Duration, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
@@ -8,11 +9,26 @@ export class ProductsAppStack extends Stack {
   //exportar função para usar em outra stack ou passa por uma api gateway (o que é o que queremos aqui!!!)
   readonly prodructsFetchHandler: lambdaNodeJS.NodejsFunction;
 
+  readonly productsDdb: Table;
+
   // scope - onde vai ser inserido
   // props - algumas propriedades
   // id a id dessa parada memo
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    //Create a table on dynamoDb
+    this.productsDdb = new Table(this, "ProductsDdb", {
+      tableName: "products",
+      removalPolicy: RemovalPolicy.DESTROY, //Politica de destruição da tabela ao destruir uma task - geralmente eh mantido
+      partitionKey: {
+        name: "id",
+        type: AttributeType.STRING,
+      },
+      billingMode: BillingMode.PROVISIONED,
+      readCapacity: 1,
+      writeCapacity: 1,
+    });
 
     //criação da lambda products
     this.prodructsFetchHandler = new lambdaNodeJS.NodejsFunction(
@@ -29,7 +45,13 @@ export class ProductsAppStack extends Stack {
           minify: true, //deixar a função lambda mais otimizada possivel
           sourceMap: false,
         },
+        //pegar o nome da tabela por .env
+        environment: {
+          PRODUCTS_DDB: this.productsDdb.tableName,
+        },
       }
     );
+    //permitir que a tabela possa ser lida por prodructsFetchHandler
+    this.productsDdb.grantReadData(this.prodructsFetchHandler);
   }
 }
